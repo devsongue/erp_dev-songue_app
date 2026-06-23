c8814be2fdea49a8a8092f870ff96447c9a25b1f
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-06-23 11:41:07 +0000

    fix

--- _mv.ps1
+++
@@ -1,9 +0,0 @@
-New-Item -ItemType Directory -Force -Path src\routes\$companySlug
-
-$files = Get-ChildItem -Path src\routes -File | Where-Object { 
-  $_.Name -notin @('__root.tsx', 'index.tsx', 'signup.tsx', 'pricing.tsx', 'users.tsx') -and $_.Name -like '*.tsx' 
-}
-
-foreach ($file in $files) {
-  Move-Item -Path $file.FullName -Destination src\routes\$companySlug\
-}
